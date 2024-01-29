7a1350a663e8662af81c0a1f68cbdbfff0e9dc31
Risonaldo <risonaldomaciel@hotmail.com>
2024-01-29 13:16:13 -0400

    ediarista padrao de projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+@section('content')
+    <p>Bem vindo ao E-Diariasta </p>
+@stop
